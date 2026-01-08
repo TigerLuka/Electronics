53ac6251eb0eebc7d48a2b72de6b6af561e47999
Luka Brekalo <lukabrekalo49@gmail.com>
2026-01-08 01:08:30 -0600

    prechaarge
    fixed precharge footprin

--- PrechargeDischarge/Precharge26/code/precharge/precharge.ino
+++ PrechargeDischarge/Precharge26/code/precharge/precharge.ino
@@ -147,7 +147,7 @@ void precharge() {
   // If a precharge is detected faster than this, an error is
   // thrown - assumed wiring fault. This could also arrest oscillating or
   // chattering AIRs, because the TS will retain some amount of precharge.
-  const float TARGET_PERCENT = 88.0;   // TODO: Requires suitable value during commissioning (eg 95%)
+  const float TARGET_PERCENT = 95.0;   // TODO: Requires suitable value during commissioning (eg 95%)
   const unsigned int SETTLING_TIME = 200; // [ms] Precharge amount must be over TARGET_PERCENT for this long before we consider precharge complete
   static unsigned long epoch;
   static unsigned long tStartPre;
